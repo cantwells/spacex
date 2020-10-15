b3b7788612ed4b8b876b54113108ed4a3776c7a8
cantwell <rjazantsev@gmail.com>
2020-10-15 14:53:25 +0300

    Output menu items from state

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css';
 import logo from '../../logo.svg';
 
-const Header = () => {
+const Header = props => {
     return( 
         <header className="header">
         <img
@@ -12,18 +12,14 @@ const Header = () => {
         />
         <nav className="main-nav nav">
           <ul className="list">
-            <li className="item">
-              <a href="/" className="item-link">Falcon 1</a>
-            </li>
-            <li className="item">
-              <a href="/" className="item-link">Falcon 9</a>
-            </li>
-            <li className="item">
-              <a href="/" className="item-link">Falcon Heavy</a>
-            </li>
-            <li className="item">
-              <a href="/" className="item-link">Updates</a>
-            </li>
+            {props.rockets.map( (item, idx) => (
+              <li key={idx} className="item">
+                <a onClick={e => {
+                  e.preventDefault();
+                  props.changeRocket(item);
+                }} href="/" className="item-link">{item}</a>
+              </li>
+            ))}
           </ul>
         </nav>
         <nav className="secondary-nav">
