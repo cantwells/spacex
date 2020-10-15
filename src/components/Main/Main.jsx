32cd670d1d514547dea8b527d6223063f44b303d
cantwell <rjazantsev@gmail.com>
2020-10-15 15:49:12 +0300

    chaged video depand on state field rocket

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './Main.css';
 
+const videos = {
+    'Falcon 1': 'moon',
+    'Falcon 9': 'earth',
+    'Falcon Heavy': 'mars',
+    other: 'space'
+}
+
 const Main = ({rocket}) => {
     return(
         <section className="main">
             <h1 className="title">{rocket}</h1>
             <div className="video-container">
-                <video className="video" autoPlay loop muted src="./video/moon.mp4"></video>
+                <video className="video" 
+                        autoPlay 
+                        loop 
+                        muted 
+                        src={`./video/${videos.hasOwnProperty(rocket) ? videos[rocket] :
+                        videos.other }.mp4`}>
+                </video>
             </div>
         </section>
     )
