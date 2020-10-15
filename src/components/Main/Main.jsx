59a4fc3e0c42688266d62fe2d18ca679a78834cf
cantwell <rjazantsev@gmail.com>
2020-10-15 14:54:55 +0300

    Output tocket name from state

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Main.css';
 
-const Main = () => {
+const Main = ({rocket}) => {
     return(
         <section className="main">
-            <h1 className="title">Falcon 1</h1>
+            <h1 className="title">{rocket}</h1>
             <div className="video-container">
                 <video className="video" autoPlay loop muted src="./video/moon.mp4"></video>
             </div>
