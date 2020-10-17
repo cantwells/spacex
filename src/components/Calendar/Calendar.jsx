72a3d356b96d34802dd4ccc3d3d97bdf3471381d
cantwell <rjazantsev@gmail.com>
2020-10-17 11:27:03 +0300

    beautify js code in Calendar

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -13,7 +13,6 @@ const Calendar = () => {
     useEffect(() => {
         fetch.getLaunches().then( data => setLaunches(data));
     }, [])
-    // console.log(launches);
     return (
         <section className="calendar">
             <div className="container">
@@ -26,7 +25,7 @@ const Calendar = () => {
                                 </div>
                                 <div className="launches-content">
                                     <h2 className="launches-title">{item.name}</h2>
-                                    <Link to={`/details?#${item.id}`} className="button launches-details">Подробнее</Link>
+                                    <Link to={`/details/${item.id}`} className="button launches-details">Подробнее</Link>
                                 </div>
                             </article>
                         </li>
