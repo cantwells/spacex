a6ccd9625b309d6ef1573a80f44b784f27f73ed6
cantwell <rjazantsev@gmail.com>
2020-10-17 11:28:07 +0300

    using params from useHistory and Youtube

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,20 +2,19 @@ import React,{useState, useEffect}  from 'react';
 import './Details.css';
 import FetchData from '../../FetchData';
 import {useHistory} from 'react-router-dom';
+import Youtube from 'react-youtube';
 
 const fetchData = new FetchData();
 
-const Details = () => {
-    
+const Details = props => {
     const history = useHistory();
-    const launchID = window.location.hash.slice(1);
-    const [launch, setLaunch] = useState([]);
+    const launchID = props.match.params.id;
+    const [launch, setLaunch] = useState(null);
     
     useEffect(() => {
         fetchData.getLaunches()
                 .then( data =>  setLaunch(data.find( item => item.id === launchID )))
     }, [launchID])
-    console.log(launch);
     if(!launch) return null;
     return (
         <section className="details">
@@ -23,14 +22,14 @@ const Details = () => {
                 <h1 className="title">{launch.name}</h1>
                 <div className="details-row">
                     <div className="details-image">
-                        <img src={launch.links ? launch.links.patch.small : ''} alt={launch.name} />
+                        <img src={launch.links.patch.small} alt={launch.name} />
                     </div>
                     <div className="details-content">
                         <p className="details-description">{launch.details}</p>
                     </div>
                 </div>
                 <div>
-                    <iframe className="details-youtube" width="560" height="315" src={launch.links ? `https://www.youtube.com/embed/${launch.links.youtube_id}` : ''} frameBorder="0" allowFullScreen></iframe>
+                    <Youtube className="details-youtube" opts={ {height: '315', width: '560'} } videoId={launch.links.youtube_id}/>
                 </div>
             </div>
                 <div onClick={ history.goBack } className="button button-back">go back</div>
